5cab3f6fcf3ae5ac72f28de26a4ff9cd13c5997b
Christofer Engel <christofer.engel@gmail.com>
2024-08-17 17:44:17 +0200

    Regenerated files.

--- dist/index.js
+++ dist/index.js
@@ -25061,9 +25061,12 @@ const generateFile_1 = __nccwpck_require__(5962);
  */
 async function run() {
     try {
-        const topdomain = core.getInput('topdomain', { required: true });
-        const version = core.getInput('version', { required: true });
-        const apiKey = core.getInput('apikey', { required: true });
+        // const topdomain = core.getInput('topdomain', { required: true });
+        // const version = core.getInput('version', { required: true });
+        // const apiKey = core.getInput('apikey', { required: true });
+        const topdomain = process.env.INPUT_TOPDOMAIN || '';
+        const version = process.env.INPUT_VERSION || '';
+        const apiKey = process.env.INPUT_APIKEY || '';
         if (topdomain === '' || version === '' || apiKey === '') {
             throw new Error("Missing required input");
         }
--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,12 @@ import { generate } from './generateFile';
  */
 export async function run() {
     try {
-        const topdomain = core.getInput('topdomain', { required: true });
-        const version = core.getInput('version', { required: true });
-        const apiKey = core.getInput('apikey', { required: true });
+        // const topdomain = core.getInput('topdomain', { required: true });
+        // const version = core.getInput('version', { required: true });
+        // const apiKey = core.getInput('apikey', { required: true });
+        const topdomain = process.env.INPUT_TOPDOMAIN || '';
+        const version = process.env.INPUT_VERSION || '';
+        const apiKey = process.env.INPUT_APIKEY || '';
 
         if (topdomain === '' || version === '' || apiKey === '') {
             throw new Error("Missing required input");
